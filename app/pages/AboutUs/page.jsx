7a2bda2fa7d00d013fc30e4078440d92e46e9aa8
Emmanuel <emmanuelmakau90@gmail.com>
2026-04-06 12:30:22 +0300

    Make UI fully responsive and improve typography

--- app/pages/AboutUs/page.jsx
+++ app/pages/AboutUs/page.jsx
@@ -489,7 +489,7 @@ export default function AboutPage() {
   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.123456!2d37.2618!3d-1.2921!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMcKwMTcnMzEuNiJTIDM3wrAxNSc0Mi41IkU!5e0!3m2!1sen!2ske!4v1234567890"
   width="100%"
   height="100%"
-  style="border:0; position:absolute; top:0; left:0; width:100%; height:100%;"
+  style={{ border: 0, position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
   allowFullScreen
   loading="lazy"
   referrerPolicy="no-referrer-when-downgrade"
