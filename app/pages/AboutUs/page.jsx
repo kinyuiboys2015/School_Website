3d0325e35b5a87cb439eb18d6e46d5b8838e658a
Emmanuel <emmanuelmakau90@gmail.com>
2026-04-06 11:21:26 +0300

    Make UI fully responsive and improve typography

--- app/pages/AboutUs/page.jsx
+++ app/pages/AboutUs/page.jsx
@@ -125,12 +125,12 @@ export default function AboutPage() {
           </div>
 
           {/* Stats Bar */}
-          <div className="mt-10 sm:mt-14 grid grid-cols-2 md:grid-cols-4 gap-3 sm:gap-4">
-            <AnimatedCounter value="400+" label="Students" icon={Users} />
-            <AnimatedCounter value="88" label="Completion Rate" icon={TrendingUp} suffix="%" />
-            <AnimatedCounter value="45" label="Awards Won" icon={Trophy} />
-            <AnimatedCounter value="12" label="Curriculums" icon={BookOpen} />
-          </div>
+         <div className="mt-10 sm:mt-14 grid grid-cols-2 md:grid-cols-4 gap-3 sm:gap-4">
+  <AnimatedCounter value="400" label="Students" icon={Users} suffix="+" />
+  <AnimatedCounter value="88" label="Completion Rate" icon={TrendingUp} suffix="%" />
+  <AnimatedCounter value="45" label="Awards Won" icon={Trophy} />
+  <AnimatedCounter value="12" label="Curriculums" icon={BookOpen} />
+</div>
         </div>
       </section>
 
